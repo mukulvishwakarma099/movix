1b8c3755bc9febb8f5b39e1a1d49467031bce3b8
mukulvishwakarma099 <mukulvishwakarma099@gmail.com>
2023-07-06 12:46:44 +0530

    movix app

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import PageNotFound from "./pages/404/PageNotFound";
+import Explore from "./pages/explore/Explore";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Details from "./pages/details/Details";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const dispatch = useDispatch();
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
+    });
+  };
+
+  useEffect(() => {
+    fetchApiConfig();
+    genresCall();
+  }, []);
+
+  const genresCall = async () => {
+    let promises = [];
+    let endpoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endpoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/details/carousels/Similar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
+import useFetch from "../../../hooks/useFetch";
+
+const Similar = ({ mediaType, id }) => {
+  // console.log(mediaType);
+  const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
+
+  const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
+
+  return (
+    <Carousel
+      data={data?.results}
+      loading={loading}
+      title={title}
+      endPoint={mediaType}
+    />
+  );
+};
+
+export default Similar;
